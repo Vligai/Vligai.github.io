c37f560f2cf3fc7bdeb12d59772bbd9de48fa9cd
vligai <vligai@stevens.edu>
2016-12-22 20:18:13 -0500

    playing around

---
+++ main.js
@@ -0,0 +1,10 @@
+function switchbutton ()
+{
+  if(document.getElementById("myImage").src = "Capture.png")
+  {  document.getElementById("myImage").src="Capture2.gif";
+  }
+    else 
+        {
+            document.getElementById("myImage").src='Capture.png';
+        }
+}
